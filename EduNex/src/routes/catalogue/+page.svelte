e0f4c727ed30a45f27a15f9eda14bcac278eeac5
Alex Alves <alex.conceicao@edu.vs.ch>
2025-05-06 09:37:29 +0200

    Create page catalogue

--- EduNex/src/routes/catalogue/+page.svelte
+++ EduNex/src/routes/catalogue/+page.svelte
@@ -1,11 +1,5 @@
 <section class="ml-5 rounded-b-full rounded-l-full bg-[#6b1f1b] bg-blend-multiply">
 	<div class="mx-auto max-w-screen-xl px-4 py-24 text-center lg:py-56">
-		<h1
-			class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl"
-		>
-			Chercher le <span class="border-b-8 border-[#ffdedc] italic">Cours</span> qui vous correspond
-		</h1>
-		<br />
 		<div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
 			<form action="" class="mt-12 w-full">
 				<div
