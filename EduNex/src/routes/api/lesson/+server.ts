a6fc4b1903d5278be4006a39f8101a0b0c863f4c
unknown <Student@SIOP0226-EDU-12.EPTM.lan>
2025-05-20 09:05:59 +0200

    Added route to display all lessons

--- EduNex/src/routes/api/lesson/+server.ts
+++ EduNex/src/routes/api/lesson/+server.ts
@@ -1,10 +1,18 @@
-import { json } from "@sveltejs/kit";
+import mysql from 'mysql2/promise';
+import { json } from '@sveltejs/kit';
 // import { PGSQL_CONNECTION } from "$env/static/private";
 
 interface getInterface {
     url : URL;
 }
 
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Pa$$w0rd',
+  database: 'edunex',
+  port: 3306
+});
 
 /**
  * Retourne soit toutes les boites, soit certaines boites basé 
@@ -15,17 +23,13 @@ interface getInterface {
  * @returns La réponse en format JSON
  */
 export async function GET({ url } : getInterface) {
-    
     try {
-        let test = {
-            name: "hello",
-            age: "world!"
-        }
-
-        return json(test)
-
-    } catch (error) {
-        return json(error);
+        const [rows] = await connection.execute('SELECT * FROM lesson');
+        return json(rows);
+    } catch (error : any) {
+        return json({ error: error.message });
+    } finally {
+        await connection.end();
     }
     
 }
